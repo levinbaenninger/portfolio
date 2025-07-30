d1bb758521446a8e17681fd3b87dd77bb4686f55
Levin Bänninger <l.baenninger@icloud.com>
2025-07-30 20:03:05 +0200

    chore: remove bundle analyzer plugin

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,4 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {};
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
-export default withBundleAnalyzer(nextConfig);
+export default nextConfig;
