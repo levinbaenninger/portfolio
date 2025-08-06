8d5a4ed7dc0dff1a79175c343c3b23ab676c694d
Levin Bänninger <l.baenninger@icloud.com>
2025-08-06 19:32:05 +0200

    chore: configure dependabot (#1)

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,7 @@
  * @type {import('@commitlint/types').UserConfig}
  */
 const config = {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
 };
 
 export default config;
