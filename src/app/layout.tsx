1ca415c6b914025b205d65fe7c3f9f9295f15aa7
Levin Bänninger <l.baenninger@icloud.com>
2025-08-22 22:24:55 +0200

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "@once-ui-system/core/css/styles.css";
 import "@once-ui-system/core/css/tokens.css";
 
+import { Footer, Header, Providers } from "@/components";
+import { baseURL, dataStyle, effects, fonts, home, style } from "@/resources";
 import {
   Background,
   Column,
@@ -10,9 +12,8 @@ import {
   RevealFx,
   type SpacingToken,
 } from "@once-ui-system/core";
+import { Analytics } from "@vercel/analytics/next";
 import classNames from "classnames";
-import { Footer, Header, Providers } from "@/components";
-import { baseURL, dataStyle, effects, fonts, home, style } from "@/resources";
 
 export async function generateMetadata() {
   return Meta.generate({
@@ -165,6 +166,7 @@ export default async function RootLayout({
           <Footer />
         </Column>
       </Providers>
+      <Analytics />
     </Flex>
   );
 }
